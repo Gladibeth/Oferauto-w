0e159dd5630197173b76557ff4e2fc9fc64b10e4
patricia <yively.pa@gmail.com>
2019-11-02 00:37:19 -0400

    secciones new -mas buscados, -CTA , Footer , Vista de detalles

--- app/js/setting-slick.js
+++ app/js/setting-slick.js
@@ -50,3 +50,60 @@ $('.slider-nav').slick({
   arrows: true,
   focusOnSelect: true
 });
+
+$('.slider-for-details').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav-details'
+});
+$('.slider-nav-details').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for-details',
+  dots: false,
+  arrows: false,
+  focusOnSelect: true
+});
+
+
+
+
+
+$('.main-buscado__content').slick({
+  infinite: true,
+  autoplay: false,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: true,
+  responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 1000
+      }
+    }
+  ]
+});
